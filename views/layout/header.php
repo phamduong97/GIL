572246ea24549ab74c719886bbe79749a60b7338
Yumi <caubedondoc.ls01@gmail.com>
2019-02-17 15:54:21 +0700

    Tách layout ra từng phần

---
+++ views/layout/header.php
@@ -0,0 +1,22 @@
+<div id="header">
+    <div class="container-fluid">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3" id="logo">
+                    <a href=""><img src="assets/image/logo.png" alt=""></a>
+                </div>
+                <div class="col-lg-6" id="search-box">
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <button class="btn btn-default "><i class="fa fa-search"></i></button>
+                        </div>
+                        <input type="text" name="" id="inputsearch" class="form-control">
+                    </div>
+                </div>
+                <div class="col-lg-3" id="login">
+                    <a href="?controller=account&action=login">Đăng nhập</a> | <a href="register.html">Đăng ký</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
